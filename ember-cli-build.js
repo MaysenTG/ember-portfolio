0811f7e382edfd9d3b39e2629911623ca34ee753
MaysenAtHSO <maysen.greenwood@henryscheinone.co.nz>
2024-06-07 23:49:00 +1200

    Linting things

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,6 +1,6 @@
-'use strict';
+'use strict'
 
-const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const EmberApp = require('ember-cli/lib/broccoli/ember-app')
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
@@ -12,7 +12,7 @@ module.exports = function (defaults) {
     'ember-cli-babel': {
       includePolyfill: true,
     },
-  });
+  })
 
-  return app.toTree();
-};
+  return app.toTree()
+}
