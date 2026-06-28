c50375d2f4e1cefc1c235c73d16a3de6361808ac
Maysen <maysen.greenwood@henryscheinone.com>
2026-06-28 22:52:23 +1200

    Redesign website

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,20 +1,26 @@
 import Component from '@glimmer/component'
+import { registerDestructor } from '@ember/destroyable'
 
 export default class NavbarComponent extends Component {
   constructor() {
     super(...arguments)
+    this.handleScroll = this.handleScroll.bind(this)
     window.addEventListener('scroll', this.handleScroll)
+    this.handleScroll()
+
+    registerDestructor(this, () => {
+      window.removeEventListener('scroll', this.handleScroll)
+    })
   }
 
   handleScroll() {
-    window.onscroll = function () {
-      if (window.scrollY <= 100) {
-        document.querySelector('nav.navbar').style.backdropFilter = 'blur(0px)'
-        document.querySelector('nav.navbar').style.background = null
-      } else {
-        document.querySelector('nav.navbar').style.backdropFilter = 'blur(5px)'
-        document.querySelector('nav.navbar').style.background = 'rgba(5, 10, 48, 0.7)'
-      }
+    const nav = document.querySelector('nav.navbar')
+    if (!nav) return
+
+    if (window.scrollY > 100) {
+      nav.classList.add('navbar--scrolled')
+    } else {
+      nav.classList.remove('navbar--scrolled')
     }
   }
 }
---
+++ app/components/sections/about-me.js
@@ -0,0 +1,11 @@
+import Component from '@glimmer/component'
+
+export default class AboutMeComponent extends Component {
+  skills = [
+    'Javascript / Typescript',
+    'Ruby on Rails',
+    'C# / ASP.NET',
+    'Database design',
+    'Google Cloud',
+  ]
+}
